75a04e7064761476c269b9c36a6976c57d335b7b
wandefulha <1139746716@qq.com>
2023-02-19 16:21:05 +0800

    update

--- demo3/src/main/java/org/example/App.java
+++ demo3/src/main/java/org/example/App.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import org.example.mapper.HelloMapper;
+import org.example.domain.Hello;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ClassPathXmlApplicationContext applicationContext=new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        HelloMapper helloMapper=applicationContext.getBean(HelloMapper.class);
+        List<Hello> all=helloMapper.getHellos();
+        System.out.println(all.get(0));
     }
 }
---
+++ demo3/src/main/java/org/example/mapper/HelloMapper.java
@@ -0,0 +1,9 @@
+package org.example.mapper;
+
+import org.example.domain.Hello;
+
+import java.util.List;
+
+public interface HelloMapper {
+    List<Hello> getHellos();
+}
